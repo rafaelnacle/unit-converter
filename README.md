6404fe78ec3db2f0f69af414892d88aa45b73a83
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2023-09-27 21:07:00 -0300

    unit converter completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# unit-converter
+# Unit Converter
+
+A basic, but cool project for making our life easier by converting common units.
+
+## Features:
+It's pretty self-explanatory, but you can convert the following:
+- Meter to Feet
+- Liters to Gallons
+- Kilograms to Pounds
+
+It also shows the other way around! Have fun!
+
+![image](https://github.com/rafaelnacle/unit-converter/assets/54647722/5f331fd3-d6db-474b-bb98-b42c2e6cc4a2)
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+const unitEl = document.getElementById('unit-input');
+const lengthEl = document.getElementById('length');
+const volumeEl = document.getElementById('volume');
+const massEl = document.getElementById('mass');
+const convertBtn = document.getElementById('convert-btn');
+const errorEl = document.getElementById('error');
+
+lengthEl.textContent = 0;
+volumeEl.textContent = 0;
+massEl.textContent = 0;
+
+let unit = 0;
+convert(unit);
+
+convertBtn.addEventListener('click', function () {
+  unit = unitEl.value;
+
+  if (unit > 0) {
+    convert(unit);
+    errorEl.textContent = 'Insert a number to convert';
+  } else {
+    errorEl.textContent = 'Please insert a positive number';
+  }
+});
+
+function convert(unit) {
+  convertLength(unit);
+  convertVolume(unit);
+  convertMass(unit);
+}
+
+function convertLength(unit) {
+  let meterToFeet = unit * 3.28084;
+  let feetToMeter = unit * 0.3048;
+
+  return (lengthEl.textContent = `${unit} meters = ${meterToFeet.toFixed(
+    3
+  )} feet | ${unit} feet = ${feetToMeter.toFixed(3)} meters`);
+}
+function convertVolume(unit) {
+  let literToGallon = unit * 0.264172;
+  let gallonToLiters = unit * 3.78541;
+
+  return (volumeEl.textContent = `${unit} liters = ${literToGallon.toFixed(
+    3
+  )} gallons | ${unit} gallons = ${gallonToLiters.toFixed(3)} liters`);
+}
+function convertMass(unit) {
+  let kgToPounds = unit * 2.20462;
+  let poundsToKg = unit * 0.453592;
+
+  return (massEl.textContent = `${unit} kilos = ${kgToPounds.toFixed(
+    3
+  )} pounds | ${unit} pounds = ${poundsToKg.toFixed(3)} kilos`);
+}
